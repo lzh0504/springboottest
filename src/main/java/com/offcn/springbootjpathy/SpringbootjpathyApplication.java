7626b6b2587086c8ef767c490182b2ab584f8a4f
lzh <18211927120@163.com>
2020-05-12 21:38:40 +0800

    正式版本上线

--- src/main/java/com/offcn/springbootjpathy/SpringbootjpathyApplication.java
+++ src/main/java/com/offcn/springbootjpathy/SpringbootjpathyApplication.java
@@ -8,8 +8,7 @@ public class SpringbootjpathyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjpathyApplication.class, args);
-        System.out.println("我是测试1.0版本");
-        System.out.println("我是测试2.0版本");
+        System.out.println("正式服上綫了");
     }
 
 }
